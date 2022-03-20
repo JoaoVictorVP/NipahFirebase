831048505932503f3d0430f7f0171396a7157613
João Victor Viana Pacheco <joaovitorsl2012@hotmail.com>
2022-03-20 19:07:59 -0300

    * ShallowAttribute is now deprecated

--- FirebaseCore/Attributes/ShallowAttribute.cs
+++ FirebaseCore/Attributes/ShallowAttribute.cs
@@ -4,6 +4,7 @@
 /// Makes this field or property shallow by default
 /// </summary>
 [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+[Obsolete("Don't use this, instead, the Source Generator and handmade implementations should work just fine!", true)]
 public class ShallowAttribute : Attribute
 {
 
